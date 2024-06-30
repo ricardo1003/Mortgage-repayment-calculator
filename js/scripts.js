5ed35b14d65401e69bbfeca1250cb05a271369cf
unknown <ricardodonado1105@gmail.com>
2024-06-30 15:34:18 -0500

    getting DOM elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("hello world!")
+const form = document.getElementsByTagName("form")[0]
+const button = document.getElementsByTagName("button")[0]
+const resultsMonthly = document.getElementById("monthlyRepayment")
+const totalResults = document.getElementById("totalRepayment")
+const radioButtons = [...document.getElementsByClassName("radioContainer")]
+const radioInputs = [...document.getElementsByClassName("radioButton")]
+const radioLabel = [...document.getElementsByClassName("radioLabel")]
+
