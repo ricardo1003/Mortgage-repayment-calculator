01c9c5c018f16bbeda50dd334f87b441bc0e3776
unknown <ricardodonado1105@gmail.com>
2024-06-30 15:35:38 -0500

    repayments formula coded

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,28 @@ const radioButtons = [...document.getElementsByClassName("radioContainer")]
 const radioInputs = [...document.getElementsByClassName("radioButton")]
 const radioLabel = [...document.getElementsByClassName("radioLabel")]
 
+let amount = form[0].value
+let term = form[1].value
+let interest = form[2].value
+
+
+function calculateRepayment(amount, term, interest){
+    return amount*(((interest/12/100)*(1+(interest/12/100))**(term*12))/(((1+(interest/12/100))**(term*12))-1))
+}
+function calculateInterest(amount, interest){
+    return amount * (interest/100)
+}
+function totalMortgage(term){
+    return calculateRepayment(amount, term, interest) * (term*12)
+}
+function totalInterest(term){
+    return calculateInterest(amount, term, interest)*(term*12)
+}
+
+for(let i=0; i<3; i++){
+    form[i].addEventListener("input", ()=>{
+        amount = form[0].value
+        term = form[1].value
+        interest = form[2].value
+    })
+}
